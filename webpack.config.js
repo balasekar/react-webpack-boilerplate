5e08a133a3561179a557a3ef1d21f5fad41429af
balasekar <baalasekar@gmail.com>
2017-09-07 16:08:17 +0100

    Added HtmlWebpackPluginConfig

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,14 @@
     ./webpack.config.js
 */
 const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: './client/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+
 module.exports = {
     entry: './client/index.js',
     output: {
@@ -13,5 +21,6 @@ module.exports = {
             { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
             { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
         ]
-    }
-}
+    },
+    plugins: [HtmlWebpackPluginConfig]
+};
