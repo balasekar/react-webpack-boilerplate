cdd6f7daa9e912bf1633b92f38ac673a33dd58dd
balasekar <baalasekar@gmail.com>
2017-09-07 16:18:35 +0100

    Hello world React app

--- client/index.js
+++ client/index.js
@@ -1,1 +1,5 @@
-console.log('Hello world');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
